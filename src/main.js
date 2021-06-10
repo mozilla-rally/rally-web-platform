2b0709479d6ee5e92cac2928f31ff2fb98e13c4c
Robert Helmer <rhelmer@rhelmer.org>
2021-06-09 18:46:09 -0700

    note about why majority of study is disabled

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
+import runStudy from "./study.js";
 import { setupAuth, googleSignIn, emailSignIn } from "./auth.js";
-//import runStudy from "./study.js";
 
-// runStudy(__ENABLE_DEVELOPER_MODE__);
 setupAuth();
-let port;
-chrome.runtime.onConnect.addListener(p => {
-    port = p;
-    p.onMessage.addListener(message => {
+
+// Listen for login messages from the options UI.
+chrome.runtime.onConnect.addListener(port => {
+    // NOTE - `port` is bi-directional, if we want to use it to communicate back to the options page we can.
+    port.onMessage.addListener(message => {
         if ("email" in message && "password" in message) {
             emailSignIn(message);
         } else if ("provider" in message && message.provider === "google") {
@@ -14,4 +14,6 @@ chrome.runtime.onConnect.addListener(p => {
         }
     });
 });
-// emailSignIn({ email: "test@example.com", password: "test" });
+
+// FIXME - our webextension polyfill doesn't seem to be working, getting errors about `browser.*` missing in Chrome?!
+// runStudy(__ENABLE_DEVELOPER_MODE__);
