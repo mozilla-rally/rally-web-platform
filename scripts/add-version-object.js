0efc3827ba22dfd7340383234e5a4d5e3f1238f6
hamilton <hamilton.ulmer@gmail.com>
2021-08-26 07:39:15 -0700

    changes to basic iso versrion

--- scripts/add-version-object.js
+++ scripts/add-version-object.js
@@ -3,6 +3,24 @@ import fs from "fs";
 
 const pkg = JSON.parse(fs.readFileSync("./package.json").toString());
 
+function toIsoString(date) {
+    const tzo = -date.getTimezoneOffset();
+    const dif = tzo >= 0 ? '+' : '-';
+    const pad = function(num) {
+    const norm = Math.floor(Math.abs(num));
+        return (norm < 10 ? '0' : '') + norm;
+    };
+
+    return date.getFullYear() +
+        '-' + pad(date.getMonth() + 1) +
+        '-' + pad(date.getDate()) +
+        'T' + pad(date.getHours()) +
+        ':' + pad(date.getMinutes()) +
+        ':' + pad(date.getSeconds()) +
+        dif + pad(tzo / 60) +
+        ':' + pad(tzo % 60);
+}
+
 const version = {
     version: pkg.version,
     repository: pkg.repository.url,
@@ -10,8 +28,7 @@ const version = {
     commit:  cp
     .execSync('git rev-parse HEAD')
     .toString().trim(),
-    // sweden uses ISO format
-    buildDate: (new Date()).toLocaleString('sv',  { timeZoneName: 'short' })
+    buildDate: toIsoString(new Date())
 }
 
 fs.writeFileSync("static/version.json", JSON.stringify(version));
