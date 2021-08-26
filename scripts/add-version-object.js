95be0f23008d8aad906f6f8649009f3fbba9d79b
hamilton <hamilton.ulmer@gmail.com>
2021-08-26 11:39:48 -0700

    stop being fancy & just use .toISOString() like the rest of the world

--- scripts/add-version-object.js
+++ scripts/add-version-object.js
@@ -3,24 +3,6 @@ import fs from "fs";
 
 const pkg = JSON.parse(fs.readFileSync("./package.json").toString());
 
-function toIsoString(date) {
-    const tzo = -date.getTimezoneOffset();
-    const dif = tzo >= 0 ? '+' : '-';
-    const pad = function(num) {
-    const norm = Math.floor(Math.abs(num));
-        return (norm < 10 ? '0' : '') + norm;
-    };
-
-    return date.getFullYear() +
-        '-' + pad(date.getMonth() + 1) +
-        '-' + pad(date.getDate()) +
-        'T' + pad(date.getHours()) +
-        ':' + pad(date.getMinutes()) +
-        ':' + pad(date.getSeconds()) +
-        dif + pad(tzo / 60) +
-        ':' + pad(tzo % 60);
-}
-
 const version = {
     version: pkg.version,
     repository: pkg.repository.url,
@@ -28,7 +10,7 @@ const version = {
     commit:  cp
     .execSync('git rev-parse HEAD')
     .toString().trim(),
-    buildDate: toIsoString(new Date())
+    buildDate: (new Date()).toISOString()
 }
 
 fs.writeFileSync("static/version.json", JSON.stringify(version));
