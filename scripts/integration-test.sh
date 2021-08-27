d4507d298dd0ecf68ee0bc186afc90cb367d06cc
Robert Helmer <rhelmer@rhelmer.org>
2021-08-27 10:21:09 -0700

    leave Chrome non-headless commented out, but fix command to make it easier to enable later

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -8,4 +8,4 @@ firebase emulators:exec "npm run test:integration:jest -- --test_browser=firefox
 firebase emulators:exec "npm run test:integration:jest -- --test_browser=chrome --load_extension=false --headless_mode=true" --import tests/integration/testdata 2>&1 | tee integration.log
 
 # FIXME Chrome Headless mode does not support extensions, need to set up a display server if we want this to work.
-# ./node_modules/.bin/firebase emulators:exec "npm run test:integration:jest --test_browser=chrome --load_extension=talse --headless_mode=false" --import tests/integration/testdata 2>&1 | tee integration.log
+# firebase emulators:exec "npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false" --import tests/integration/testdata 2>&1 | tee integration.log
