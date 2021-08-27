f2e170dfb3b7c2ef99e025f50944cf997db1ca0a
Robert Helmer <rhelmer@rhelmer.org>
2021-08-27 13:51:16 -0700

    study add admin script, might move this to a cloud function

--- scripts/add-studies-to-firebase.ts
+++ scripts/add-studies-to-firebase.ts
@@ -1,28 +1,21 @@
 import admin from "firebase-admin";
-import { setDoc, collection } from "firebase/firestore";
 
-import serviceAccount from "../serviceAccount.json";
-import studies from "../data/studies.json";
+import { studies } from "../data/studies";
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  // databaseURL: "https://rally-web-spike.firebaseio.com"
-  databaseURL: "http://localhost:9099"
+  credential: admin.credential.applicationDefault(),
 });
 
-
-function addStudiesToFirebase() {
-  admin.firestore()
-    .doc(path.join('/'))
-    .set(data)
-    .then(() => console.log(`Document ${path.join('/')} uploaded.`))
-    .catch(() => console.error(`Could not write document ${path.join('/')}.`));
-}
-studies.forEach(study => {
-  setDoc(collection(db, "studies", study.addonId), { merge: true });
-});
-return studies;
+function addStudyToFirebase(index, study) {
+  admin
+    .firestore()
+    .collection("studies")
+    .doc(index)
+    .set(study, { merge: true })
 }
 
-addStudiesToFirebase();
+for (const [index, study] of Object.entries(studies)) {
+  console.info(`Loading study ${index} into Firestore`);
+  addStudyToFirebase(index, study);
+}
 
