4602b74479607b82ae638e7912e3401af510bdf1
Robert Helmer <rhelmer@rhelmer.org>
2021-09-01 12:00:30 -0700

    squelch preventAssignment message

--- svelte.config.js
+++ svelte.config.js
@@ -18,6 +18,7 @@ const config = {
 		vite: {
 			plugins: [
 				replace({
+					preventAssignment: true,
 					// the following replacements build the site URLs.
 					// In the templates, use (for example) __BASE_SITE_URL__/__FAQ_PATH__
 					__BASE_SITE__: "https://rally.mozilla.org",
