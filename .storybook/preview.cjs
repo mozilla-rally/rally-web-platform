7403db3f54cad6ce6506d9722542d1c86748a64b
Carla Severe <csevere@mozilla.com>
2021-09-02 12:22:45 -0400

    set up

---
+++ .storybook/preview.cjs
@@ -0,0 +1,14 @@
+export const parameters = {
+  actions: {
+    argTypesRegex: "^on[A-Z].*",
+    argTypesRegex: "^fetch[A-Z].*",
+    argTypesRegex: "^create[A-Z].*",
+    argTypesRegex: "^update[A-Z].*",
+  },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
