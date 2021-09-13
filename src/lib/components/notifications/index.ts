2f9691430b796b29112ec37f32d8a90273513be6
Robert Helmer <rhelmer@rhelmer.org>
2021-09-12 21:42:40 -0700

    must use import type if importsNotUsedAsValues is set to error in tsconfig

--- src/lib/components/notifications/index.ts
+++ src/lib/components/notifications/index.ts
@@ -2,7 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 import type { SvelteComponent } from "svelte";
-import { writable, derived, Readable } from "svelte/store";
+import { writable, derived } from "svelte/store";
+import type { Readable } from "svelte/store";
 
 const NOTIFICATION_TIMEOUT = 2000;
 
