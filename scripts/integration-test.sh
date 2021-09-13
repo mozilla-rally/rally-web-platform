6a335719e4b6b02945c55117a333101276ea49c2
Robert Helmer <rhelmer@rhelmer.org>
2021-09-12 21:43:11 -0700

    keep the chrome extension version test working even though it's temporarily disabled

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -17,4 +17,4 @@ echo "Testing Chrome, no extension"
 firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=chrome --load_extension=false --headless_mode=true" 2>&1 | tee integration.log
 
 # FIXME Chrome Headless mode does not support extensions, need to set up a display server if we want this to work.
-# firebase emulators:exec "npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false" 2>&1 | tee integration.log
+# firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false" 2>&1 | tee integration.log
