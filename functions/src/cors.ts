66e95526c5d06c1083195beab237580ed8bdde6d
Ashwin Agarwal <me@ashwinagarwal.com>
2022-04-21 12:00:35 -0400

    Remove explicit true/false in cors.ts

--- functions/src/cors.ts
+++ functions/src/cors.ts
@@ -2,5 +2,5 @@ import cors from "cors";
 
 export const useCors = cors({
   // Don't use CORS in testing mode
-  origin: process.env.NODE_ENV !== "test" ? true : false,
+  origin: process.env.NODE_ENV !== "test",
 });
