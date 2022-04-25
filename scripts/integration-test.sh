422e9adf53ab93beb5924edcde0dae9d42db58e3
Robert Helmer <rhelmer@rhelmer.org>
2022-04-25 09:45:39 -0700

    temporarily disable chrome functional tests

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -13,8 +13,8 @@ echo "Testing Firefox, no extension"
 firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=firefox --load_extension=false --headless_mode=true" 2>&1 | tee integration.log
 echo "Testing Firefox with extension"
 firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=firefox --load_extension=true --headless_mode=true"  2>&1 | tee integration.log
-echo "Testing Chrome, no extension"
-firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=chrome --load_extension=false --headless_mode=true" 2>&1 | tee integration.log
+# echo "Testing Chrome, no extension"
+# firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=chrome --load_extension=false --headless_mode=true" 2>&1 | tee integration.log
 
 # FIXME Chrome Headless mode does not support extensions, need to set up a display server if we want this to work.
 # firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=chrome --load_extension=true --headless_mode=false" 2>&1 | tee integration.log
