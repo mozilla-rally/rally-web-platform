a9a03153ed656e2e9c59f9a6d97311e3f0210d68
Ashwin Agarwal <me@ashwinagarwal.com>
2022-05-18 15:09:53 -0500

    Add Cloud Task enqueues [NOT WORKING

--- functions/src/glean.ts
+++ functions/src/glean.ts
@@ -1,5 +1,6 @@
 import Glean from "@mozilla/glean/node";
 import PingEncryptionPlugin from "@mozilla/glean/plugins/encryption";
+import { CloudTasksClient } from "@google-cloud/tasks";
 import axios from "axios";
 import functions from "firebase-functions";
 import { Mutex, Semaphore, withTimeout } from "async-mutex";
@@ -28,6 +29,9 @@ import {
 const GLEAN_DEBUG_VIEW_TAG = "MozillaRally";
 const ENABLE_GLEAN =
   process.env.ENABLE_GLEAN || !process.env.FUNCTIONS_EMULATOR;
+const ENABLE_CLOUD_TASK = true;
+  // (process.env.ENABLE_GLEAN && process.env.GOOGLE_APPLICATION_CREDENTIALS) ||
+  // !process.env.FUNCTIONS_EMULATOR;
 const GLEAN_RALLY_APP_ID = process.env.FUNCTIONS_EMULATOR
   ? "test-app-id"
   : "rally-core";
@@ -41,11 +45,19 @@ const GLEAN_ENCRYPTION_JWK = {
   x: "m7Gi2YD8DgPg3zxora5iwf0DFL0JFIhjoD2BRLpg7kI",
   y: "zo35XIQME7Ct01uHK_LrMi5pZCuYDMhv8MUsSu7Eq08",
 };
+const STAGING_PROJECT_ID = "moz-fx-data-rall-nonprod-ac2a";
 const PRODUCTION_PROJECT_ID = "moz-fx-data-rally-w-prod-dfa4";
 
 const GLEAN_DEFAULT_TIMEOUT = 10000;
 const gleanLock = withTimeout(new Mutex(), GLEAN_DEFAULT_TIMEOUT); // Lock on global Glean instance, metrics, and pings
 const submitPingFlag = withTimeout(new Semaphore(1), GLEAN_DEFAULT_TIMEOUT); // Allow Glean to signal once ping is sent
+
+const cloudTasksClient = new CloudTasksClient();
+const cloudTaskParams = {
+  project: "",
+  location: "us-central-1",
+  queue: "",
+};
 
 /*
  * platformEnrollment
@@ -58,9 +70,11 @@ export async function platformEnrollment(rallyID: string): Promise<void> {
 
   rallyMetrics.id.set(rallyID);
 
+  cloudTaskParams.queue = "platformEnrollment";
   await submitPingFlag.acquire();
   rallyPings.enrollment.submit();
   await submitPingFlag.waitForUnlock();
+  cloudTaskParams.queue = "";
 
   releaseGlean();
 }
@@ -77,9 +91,11 @@ export async function platformUnenrollment(rallyID: string): Promise<void> {
 
   rallyMetrics.id.set(rallyID);
 
+  cloudTaskParams.queue = "platformUnnrollment";
   await submitPingFlag.acquire();
   rallyPings.unenrollment.submit();
   await submitPingFlag.waitForUnlock();
+  cloudTaskParams.queue = "";
 
   releaseGlean();
 }
@@ -99,9 +115,11 @@ export async function demographics(
   rallyMetrics.id.set(rallyID);
   setUserMetrics(demographicsData);
 
+  cloudTaskParams.queue = "demographics";
   await submitPingFlag.acquire();
   rallyPings.demographics.submit();
   await submitPingFlag.waitForUnlock();
+  cloudTaskParams.queue = "";
 
   releaseGlean();
 }
@@ -123,9 +141,11 @@ export async function studyEnrollment(
   enrollmentMetrics.studyId.set(studyID);
   enrollmentMetrics.schemaNamespace.set(schemaNamespace);
 
+  cloudTaskParams.queue = "studyEnrollment";
   await submitPingFlag.acquire();
   rallyPings.studyEnrollment.submit();
   await submitPingFlag.waitForUnlock();
+  cloudTaskParams.queue = "";
 
   releaseGlean();
 }
@@ -147,9 +167,11 @@ export async function studyUnenrollment(
   unenrollmentMetrics.studyId.set(studyID);
   unenrollmentMetrics.schemaNamespace.set(schemaNamespace);
 
+  cloudTaskParams.queue = "studyUnenrollment";
   await submitPingFlag.acquire();
   rallyPings.studyUnenrollment.submit();
   await submitPingFlag.waitForUnlock();
+  cloudTaskParams.queue = "";
 
   releaseGlean();
 }
@@ -172,8 +194,11 @@ function initializeGlean(): void {
     appChannel = projectID === PRODUCTION_PROJECT_ID ? "PRODUCTION" : "STAGING";
   }
 
-  if (appChannel !== "PRODUCTION") {
+  if (appChannel == "PRODUCTION") {
+    cloudTaskParams.project = PRODUCTION_PROJECT_ID;
+  } else {
     Glean.setSourceTags(["automation"]); // discard non-production pings from non-Live Views
+    cloudTaskParams.project = STAGING_PROJECT_ID;
   }
 
   // Glean.initialize is a no-op if Glean is already initialized
@@ -189,7 +214,8 @@ function initializeGlean(): void {
  * Helper function for setting user metrics
  * from demographic data (mapping)
  */
-function setUserMetrics(data: any): void { // eslint-disable-line @typescript-eslint/no-explicit-any
+function setUserMetrics(data: any): void {
+  // eslint-disable-line @typescript-eslint/no-explicit-any
   if (!data) return;
 
   if ("age" in data) {
@@ -219,7 +245,8 @@ function setUserMetrics(data: any): void { // eslint-disable-line @typescript-es
   }
 
   if ("school" in data) {
-    const KEY_FIXUP: any = { // eslint-disable-line @typescript-eslint/no-explicit-any
+    const KEY_FIXUP: any = {
+      // eslint-disable-line @typescript-eslint/no-explicit-any
       high_school_graduate_or_equivalent: "high_school_grad_or_eq",
       some_college_but_no_degree_or_in_progress: "college_degree_in_progress",
     };
@@ -249,21 +276,53 @@ class CustomPingUploader extends Uploader {
     body: string | Uint8Array,
     headers: Record<string, string>
   ): Promise<UploadResult> {
-    const result = await axios
-      .post(url, body, { headers: headers })
-      .then(function (response) {
-        return {
-          status: response.status,
-          result: UploadResultStatus.Success,
-        };
-      })
-      .catch(function (error) {
-        functions.logger.error(error);
-        return {
-          status: 500,
-          result: UploadResultStatus.UnrecoverableFailure,
-        };
-      });
+    let result;
+    if (!ENABLE_CLOUD_TASK) {
+      // Upload Glean ping directly
+      functions.logger.info("Submitting Glean ping directly...");
+      result = await axios
+        .post(url, body, { headers: headers })
+        .then(function (response) {
+          return {
+            status: response.status,
+            result: UploadResultStatus.Success,
+          };
+        })
+        .catch(function (error) {
+          functions.logger.error(error);
+          return {
+            status: 500,
+            result: UploadResultStatus.UnrecoverableFailure,
+          };
+        });
+    } else {
+      // Enqueue task
+      functions.logger.info("Submitting Glean ping via Google Cloud Task...");
+      const { project, location, queue } = cloudTaskParams;
+      if (!project)
+        throw new Error("Cloud Task error: no project ID specified");
+      if (!queue) throw new Error("Cloud Task error: no queue specified");
+
+      const parent = cloudTasksClient.queuePath(project, location, queue);
+      const task = {
+        httpRequest: {
+          httpMethod: "POST" as "POST",
+          body: body,
+          headers: headers,
+          url,
+          oidcToken: {
+            serviceAccountEmail: `client@${project}.iam.gserviceaccount.com`
+          }
+        },
+      };
+      const request = { parent: parent, task: task };
+      const [response] = await cloudTasksClient.createTask(request);
+      functions.logger.info(`Created task: ${response.name}`);
+      result = {
+        status: 200,
+        result: UploadResultStatus.Success,
+      };
+    }
 
     // Signal to ping function that ping has been submitted
     submitPingFlag.release();
