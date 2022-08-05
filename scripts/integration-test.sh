cfc512d17a7b0980935e64fd54028b57f37b250b
Carla Severe <csevere@mozilla.com>
2022-08-04 21:41:04 -0400

    headless mode true

--- scripts/integration-test.sh
+++ scripts/integration-test.sh
@@ -10,9 +10,9 @@ npm install && npm run build && npm run package
 popd
 
 echo "Testing Firefox, no extension"
-firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=firefox --load_extension=false --headless_mode=false" 2>&1 | tee integration.log
+firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=firefox --load_extension=false --headless_mode=true" 2>&1 | tee integration.log
 echo "Testing Firefox with extension"
-firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=firefox --load_extension=true --headless_mode=false"  2>&1 | tee integration.log
+firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=firefox --load_extension=true --headless_mode=true"  2>&1 | tee integration.log
 # echo "Testing Chrome, no extension"
 # firebase emulators:exec --project demo-rally "npm run load:data && npm run test:integration:jest -- --test_browser=chrome --load_extension=false --headless_mode=true" 2>&1 | tee integration.log
 
